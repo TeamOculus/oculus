f18c619c5bc632883de7b9981566468d7ed3e3c0
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-12-19 15:49:14 -0700

    Completed mobile responsive dropdown header

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  clickedMenu: boolean = false;
+  clickedRift: boolean = false;
+  clickedExperiences: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+  onClickMenu() {
+    this.clickedMenu = !this.clickedMenu;
+  }
+  onClickRift() {
+    this.clickedRift = !this.clickedRift;
+  }
+  onClickExperiences() {
+    this.clickedExperiences = !this.clickedExperiences;
   }
 
 }
