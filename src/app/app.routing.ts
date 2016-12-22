3bf4b53158816160ca3c6c5c1b119c5cd27a14cb
Cameron Skidmore <cskidz99@gmail.com>
2016-12-21 17:22:48 -0700

    ready to merge again

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,7 @@ import { ExperiencesComponent } from './experiences/experiences.component';
 import { ExperiencesGearVrComponent } from './experiences/experiences-gear-vr/experiences-gear-vr.component';
 import { GearVrComponent } from './gear-vr/gear-vr.component';
 import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './login/create-account/create-account.component';
 import { RiftComponent } from './rift/rift.component';
 import { RiftReadyComponent } from './rift/rift-ready/rift-ready.component';
 
@@ -21,6 +22,7 @@ const APP_ROUTES: Routes = [
     { path: 'experiences/gear-vr', component: ExperiencesGearVrComponent },
     { path: 'gear-vr', component: GearVrComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'create-account', component: CreateAccountComponent },
     { path: 'rift', component: RiftComponent },
     { path: 'rift/ready', component: RiftReadyComponent }
 ]
