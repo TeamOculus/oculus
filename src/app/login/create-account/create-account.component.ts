7c06ae68a2d4386a3b7383cf4000bbe8bfc1f088
Henry Qi <henryqi222@gmail.com>
2016-12-22 23:35:11 -0700

    cant make http post

--- src/app/login/create-account/create-account.component.ts
+++ src/app/login/create-account/create-account.component.ts
@@ -1,25 +1,27 @@
+import { MainService } from './../../services/main.service';
 import { Component, OnInit } from '@angular/core';
-
 import {Store} from '@ngrx/store';
 import {ADD_USER} from '../../reducers/user_reducer';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrls: ['./create-account.component.scss']
+  styleUrls: ['./create-account.component.scss'],
+  providers: [MainService]
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor(private store: Store<any>) { }
+  constructor(private store: Store<any>, private mainService: MainService) { }
 
   ngOnInit() {
   }
 
-  addUser(object) {
+  addUser(userObject) {
     this.store.dispatch({
       type: ADD_USER,
-      payload: object
+      payload: userObject
     });
+    this.mainService.createUser(userObject);
   }
 
 }
